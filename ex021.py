65a23a8d73e55d163d19e95473b12cbbcc8131b5
Rogerio Campos de Oliveira <56005277+RogerioCampos007@users.noreply.github.com>
2023-01-28 20:26:14 -0300

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,14 +1,5 @@
 '''Exercício Python 021: Faça um programa em Python 
 que abra e reproduza o áudio de um arquivo MP3.'''
 
-import pygame
-
-# Inicializando o mixer PyGame
-pygame.mixer.init()
-
-# Iniciando o Pygame
-pygame.init()
-
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play(loops=0, start=0.0)
-pygame.event.wait()
+from playsound import playsound
+playsound("ex021.mp3")
