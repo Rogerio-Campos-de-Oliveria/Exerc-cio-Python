c11e2783773f4a8b0e5027f681382cd9a648c566
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-01-28 23:14:32 -0300

    Exercicios

--- ex012.py
+++ ex012.py
@@ -1,2 +1,7 @@
-print('Olá, Mundo!')
-print('Salvou tudo!')
+'''Exercício Python 12: Faça um algoritmo que leia 
+o preço de um produto e mostre seu novo preço, 
+com 5% de desconto.'''
+
+produto = float(input('Digite o valor do produto: R$ '))
+valor = produto - produto * 5 / 100
+print(f'O valor a ser pago com o desconto de 5% é de R$ {valor:.2f}')
