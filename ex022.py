bfe110e23ea6fdb505f50bef1c8c8daecdf29bf1
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-01-31 22:16:40 -0300

    Exercicios

--- ex022.py
+++ ex022.py
@@ -11,4 +11,4 @@ nome = str(input('Digite seu nome completo: ')).strip()
 print(f'Seu nome em maiúsculo é {nome.upper()}.')
 print(f'Seu nome em minúsculo é {nome.lower()}.')
 print(f'Seu nome tem ao todo {(len(nome) - nome.count(" "))} letras.')#não esquecer de usar aspas duplas quando for colocar aspas dentro de outra para diferenciar.
-print(f'Seu primeiro nome tem {nome.find(" ")} letras.')
+print(f'Seu primeiro nome tem {nome.find(" ")} letras.')#não esquecer de usar aspas duplas quando for colocar aspas dentro de outra para diferenciar.
