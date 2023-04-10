37d0edd8f610cace0c3d808fc819a46a72c3aabc
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-04-10 19:55:34 -0300

    Exercicios

---
+++ ex090.py
@@ -0,0 +1,23 @@
+'''Exercício Python 090: Faça um programa que leia nome 
+e média de um aluno, guardando também a situação em um 
+dicionário. No final, mostre o conteúdo da estrutura na 
+tela.'''
+
+
+aluno = dict()
+
+aluno['nome'] = str(input('Digite o nome do aluno: '))
+aluno['média'] = float(input(f'Digite a média do aluno {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+
+print('=-=' * 20)
+
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
+
+print('=-=' * 20)
