d137ab3a2ae6e613470efa508792b3cad9ed061e
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-04-17 22:05:54 -0300

    Exercicios

---
+++ ex101.py
@@ -0,0 +1,25 @@
+'''Exercício Python 101: Crie um programa que tenha uma 
+função chamada voto() que vai receber como parâmetro o 
+ano de nascimento de uma pessoa, retornando um valor 
+literal indicando se uma pessoa tem voto NEGADO, 
+OPCIONAL e OBRIGATÓRIO nas eleições.'''
+
+
+
+def voto(ano):
+        
+    from datetime import date
+    atual = date.today().year
+
+    idade = atual - ano
+    
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: O VOTO É OPCIONAL.'
+    else:
+        return f'Com {idade} anos: O VOTO É OBRIGATÓRIO.'
+    
+
+n = int(input('Digite o ano de nascimento: [aaaa] '))
+print(voto(n))
---
+++ ex102.py
@@ -0,0 +1,30 @@
+'''Exercício Python 102: Crie um programa que tenha uma 
+função fatorial() que receba dois parâmetros: o 
+primeiro que indique o número a calcular e outro 
+chamado show, que será um valor lógico (opcional) 
+indicando se será mostrado ou não na tela o processo de 
+cálculo do fatorial.'''
+
+def fatorial(num, show=False):
+    print('=-=' * 20)
+    f = 1
+    for c in range(num, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            if c == 1:
+                print(' = ', end='')
+        f *= c
+    
+    return f
+    
+
+n = int(input('Digite o número que deseja ver o fatorial: '))
+situação = ' '
+resp = str(input('Deseja mostrar o cálculo? [S/N] ')).upper()[0].strip()
+if resp == 'S':
+    situação = True
+else:
+    situação = False
+print(fatorial(n,show=situação))
