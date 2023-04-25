a019ef027b394c83af3ff731d30f7cf7a24f206d
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-04-24 22:26:48 -0300

    Exercicios

--- ex107/teste.py
+++ ex107/teste.py
@@ -1,7 +1,8 @@
-'''Exercício Python 107: Crie um módulo chamado moeda.py
-que tenha as funções incorporadas aumentar(), diminuir(),
-dobro() e metade(). Faça também um programa que importe esse
-módulo e use algumas dessas funções.'''
+'''Exercício Python 107: Crie um módulo chamado moeda.py 
+que tenha as funções incorporadas aumentar(), diminuir(), 
+dobro() e metade(). Faça também um programa que importe 
+esse módulo e use algumas dessas funções.'''
+
 
 from ex107 import moeda
 
--- ex110/teste.py
+++ ex110/teste.py
@@ -3,8 +3,8 @@ criadas no desafio 107 para que elas aceitem um parâmetro a
 mais, informando se o valor retornado por elas vai ser ou não 
 formatado pela função moeda(), desenvolvida no desafio 108.'''
 
+from ex110 import moeda
 
-from ex109 import moeda
 
 
 #import moeda - outra maneira
