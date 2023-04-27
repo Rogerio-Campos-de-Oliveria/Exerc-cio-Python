e028ea617ce09f923822b5209a514004da8e0feb
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-04-26 22:22:49 -0300

    Exercicio

--- ex113.py
+++ ex113.py
@@ -9,12 +9,15 @@ def leiaInt(msg):
     while True:
         try:
             n = int(input(msg))
+
         except (ValueError, TypeError):
             print('\033[31mERRO: Por favor, Digite um número inteiro válido.\033[m')
             continue
+
         except (KeyboardInterrupt):
             print('\n\033[31mUsuário prefiriu não digitar esse número.\033[m')
             return 0
+
         else:
             return n
 
@@ -23,14 +26,18 @@ def leiaFloat(msg):
     while True:
         try:
             n = float(input(msg))
+
         except (ValueError, TypeError):
             print('\033[31mERRO: Por favor, Digite um número inteiro válido.\033[m')
             continue
+
         except (KeyboardInterrupt):
             print('\n\033[31mUsuário prefiriu não digitar esse número.\033[m')
             return 0
+
         else:
             return n
+
 
 
 n1 = leiaInt('Digiete um número Inteiro: ')
