50444ac2080dde66255f775b30c0585ddb5f5352
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-05-01 23:36:40 -0300

    Exercicios

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -13,10 +13,17 @@ while True:
         lerArquivo(arq)
 
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+
     elif resposta == 3:
         cabeçalho('Saindo do sistema .... Até logo!')
         break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(1)
+
+
+
